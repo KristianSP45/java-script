26c1f033e388a202fb1c35b4138e86f7b2852c81
Kristian Pessoa <sousakristian45@gmail.com>
2025-11-24 17:51:48 -0300

    Atividade 011
    Repetições

---
+++ java-script/atividades/ex011.js
@@ -0,0 +1,29 @@
+/*
+console.log("GOOOOOOOOOOOOOL!!!")
+console.log("GOOOOOOOOOOOOOL!!!")
+console.log("GOOOOOOOOOOOOOL!!!")
+console.log("GOOOOOOOOOOOOOL!!!")
+console.log("GOOOOOOOOOOOOOL!!!")
+console.log("GOOOOOOOOOOOOOL!!!")
+*/
+var cont = 1
+while (cont <= 6) {
+    console.log("GOOOOOOOOOOOOOL!!!")
+    cont++
+}
+
+//Teste logico no inicio ^^^^^
+
+var c = 1
+do {
+    console.log("DO BRASIL!!!")
+    c++
+} while (c <= 6) 
+
+//Teste logico no final ^^^^
+
+for (var con = 0; con < 6; con++) {
+    console.log("É HEXAAAAA!!!")
+}
+
+//Teste logico com variável de controle.
