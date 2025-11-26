504a3f16c726df35c3d6cc2c6c8fadd8c0f5e024
Kristian Pessoa <sousakristian45@gmail.com>
2025-11-26 16:21:34 -0300

    Atividade 013
    Função

---
+++ java-script/atividades/ex013.js
@@ -0,0 +1,11 @@
+function parimp(n) {//paramentro
+    if (n%2 == 0) {
+        return "Par"
+    } else {
+        return "Ímpar"//resultado
+    }
+    //ação
+}
+
+let res = parimp(4) //chamada
+console.log(res)
